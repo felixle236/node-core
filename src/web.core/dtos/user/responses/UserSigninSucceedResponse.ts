490e793339b5efb7f0241e3cc05b273a6460b730
Phuoc LM <phuoclm@stageit.app>
2020-03-04 00:38:31 +0700

    Update signin response model & update packages

--- src/web.core/dtos/user/responses/UserSigninSucceedResponse.ts
+++ src/web.core/dtos/user/responses/UserSigninSucceedResponse.ts
@@ -3,12 +3,10 @@ import { UserResponse } from './UserResponse';
 
 export class UserSigninSucceedResponse {
     profile: UserResponse;
-    claims: number[];
     accessToken: string;
 
     constructor(model: User, accessToken: string) {
         this.profile = new UserResponse(model);
-        this.claims = model.role && model.role.permissions ? model.role.permissions.map(permission => permission.claim) : [];
         this.accessToken = accessToken;
     }
 }
